77a2a5d9a1399e6ece70e3fabad9f6d2c962ead1
natanesteves <natan_rv2@hotmail.com>
2022-07-12 16:19:55 -0300

    start

--- src/components/PlaceToVisit.js
+++ src/components/PlaceToVisit.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import ImageCard from './ImageCard';
 import places from '../static/places';
 import useWindowPosition from '../hook/useWindowPosition';
+
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
@@ -18,9 +21,13 @@ export default function () {
   const classes = useStyles();
   const checked = useWindowPosition('header');
   return (
+   
     <div className={classes.root} id="place-to-visit">
+    
+      
       <ImageCard place={places[1]} checked={checked} />
       <ImageCard place={places[0]} checked={checked} />
+     
     </div>
   );
 }
